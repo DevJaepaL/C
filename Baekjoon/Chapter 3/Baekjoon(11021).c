7fe39d577f6f0d0b78287501696411fb68dde5f4
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2022-06-13 14:26:21 +0900

    Add files via upload
    2022-06-13
    디렉터리 오타 수정 재업로드

---
+++ Baekjoon/Chapter 3/Baekjoon(11021).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int cnt, num1, num2;
+
+	scanf("%d", &cnt);
+	for (int i = 1; i <= cnt; ++i)
+	{
+		scanf("%d %d", &num1, &num2);
+		printf("Case #%d: %d\n", i, num1 + num2);
+	}
+	return 0;
+}
